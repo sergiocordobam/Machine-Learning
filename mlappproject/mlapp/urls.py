d665b6f4959007f82674c23c846a384c5d415496
Sergio Córdoba <scordobamuriel@gmail.com>
2022-09-20 22:20:58 -0500

    formulario Pet Classifier App

--- mlappproject/mlapp/urls.py
+++ mlappproject/mlapp/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from petclassiffier.views import home
+from petclassiffier import views as petClassifierViews
+
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("",home),
+    path("", petClassifierViews.home),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
